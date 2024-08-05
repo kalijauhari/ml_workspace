b9af97da06d95a335f6eebcdd90cc2166b7d44ee
kalijauhari <75216692+kalijauhari@users.noreply.github.com>
2024-08-06 00:34:05 +0530

    Add files via upload

--- lamda1.py
+++ lamda1.py
@@ -1,23 +1,32 @@
 import json
+import boto3
+import base64
 
-THRESHOLD = .93
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
 
-    # Grab the inferences from the event
-    inferences = event.get("inferences", [])
+    # Get the s3 address from the Step Function event input
+    key = event["s3_key"]
+    bucket = event["s3_bucket"]
 
-    # Check if any values in our inferences are above THRESHOLD
-    meets_threshold = any(inference > THRESHOLD for inference in inferences)
+    # Download the data from s3 to /tmp/image.png
+    ## TODO: fill in
+    s3.download_file(bucket, key, "/tmp/image.png")
 
-    # If our threshold is met, pass our data back out of the
-    # Step Function, else, end the Step Function with an error
-    if meets_threshold:
-        pass
-    else:
-        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
 
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
     }
